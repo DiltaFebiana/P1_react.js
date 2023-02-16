d16fdfabcfdcb6e8624ed02741eee4977d4bd4df
DiltaFebiana <febianadilta@gmail.com>
2023-02-16 12:14:47 +0700

    side effect

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 let state = {
-    inputValue: "",
+    inputValue: localStorage.getItem("inputValue") ?? "",
     hash: location.hash,
   };
   
   function setState(newState) {
-    state = { ...state, ...newState };
+    const prevState = { ...state }
+    const nextState = { ...state, ...newState };
+    state = nextState;
     render();
+    onStateChange(prevState, nextState);
+  }
+  
+  function onStateChange(prevState, nextState) {
+    if (prevState.inputValue != nextState.inputValue) {
+        localStorage.setItem("inputValue", nextState.inputValue);
+    }
+  
+    if (prevState.hash != nextState.hash) {
+        history.pushState(null, "", nextState.hash);
+    }
   }
   
   function Link(props) {
@@ -16,7 +29,6 @@ let state = {
         event.preventDefault();
         const url = new URL(event.target.href);
         setState({ hash: url.hash });
-        history.pushState(null, "", event.target.href);
         render();
     };
   
@@ -117,3 +129,4 @@ let state = {
   }
   
   render();
+  
